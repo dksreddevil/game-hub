327a2cc6b8cda7ea3bdb531518c8208d8131fb90
Dhanush Kumar <dannygnius@gmail.com>
2024-08-05 14:36:21 -0500

    Built Search Input

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,16 @@
-import { HStack,Image } from '@chakra-ui/react'
-import logo from '../assets/GameLogo.jpeg'
-import ColorModeSwitch from './ColorModeSwitch'
+import { HStack, Image } from "@chakra-ui/react";
+import logo from "../assets/GameLogo.jpeg";
+import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 
 const NavBar = () => {
   return (
-    <HStack justifyContent='space-between' padding='10px'>
-        <Image src={logo} boxSize='60px' borderRadius='25px'></Image>
-        <ColorModeSwitch></ColorModeSwitch>
+    <HStack padding="10px">
+      <Image src={logo} boxSize="60px" borderRadius="25px"></Image>
+      <SearchInput></SearchInput>
+      <ColorModeSwitch></ColorModeSwitch>
     </HStack>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
